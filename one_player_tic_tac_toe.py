a2732a6518dde2f735a60e0cca8f6b8685f10524
medha <medhachaudhary2000@gmail.com>
2021-04-22 15:06:01 +0530

    Modified

--- one_player_tic_tac_toe.py
+++ one_player_tic_tac_toe.py
@@ -1,64 +1,55 @@
 from one_player_tic_tac_toe_func import *
-
-n=1
-while (n):
-    a=['1','2','3','4','5','6','7','8','9']
-    print(a[0],' ',a[1],' ',a[2])
-    print (a[3],' ',a[4],' ',a[5])
-    print (a[6],' ',a[7],' ',a[8])
-    flag=0
+k=1
+while(k):
+    fl=0
+    count =0
+    j=0
+    print('PLAYER 1 HAS X AND COMPUTER HAS O')
+    print('AND THE GAME BEGINS!!!!')
+    a=['1','2','3']
+    b=['4','5','6']
+    c=['7','8','9']
+    print('\n',a,'\n',b,'\n',c)
+    player(a,b,c)
+    count+=1
+    print('\n',a,'\n',b,'\n',c)
+    comp1(a,b,c)
+    count+=1
+    print('\n',a,'\n',b,'\n',c)
+    player(a,b,c)
+    count+=1
+    print('\n',a,'\n',b,'\n',c)
+    check_col_pl(a,b,c,j)
+    check_row_pl(a,b,c,j)
+    check_diag_pl(a,b,c,j)
+    print(j)
+    if j==1:
+        comp1(a,b,c)
+    count+=1
+    print('\n',a,'\n',b,'\n',c)
     while 1:
-        print ("WHAT PLAYER1 WANTS TO TAKE 'X' or 'O'?")
-        ch=input()
-        if ch=='X' or ch=='O':
-            if ch=='X':
-                flagg=0
-                ch1='O'
-            else:
-                flagg=1
-                ch1='X'
+        player(a,b,c)
+        print('\n',a,'\n',b,'\n',c)
+        count+=1
+        if (check_col_cmp(a,b,c)==0 or check_row_cmp(a,b,c)==0 or check_diag_cmp(a,b,c)==0):
+            fl=1
             break
-        else:
-            print ("ONLY TWO OPTIONS AVAILABLE TO CHOOSE!")
-    for i in range(5):
-        while 1:
-            x=int(input("ENTER LOCATION YOU WANT TO PLAY\n"))
-            if x>9 or x<1:
-                print ("LOCATION NOT AVAILABLE!")
-                continue
-            elif a[x-1]=='X' or a[x-1]=='O':
-                print ("LOCATION ALREADY OCCUPIED!")
-            else:
-                a[x-1]=ch
-                break
-        print (a[0],' ',a[1],' ',a[2])
-        print (a[3],' ',a[4],' ',a[5])
-        print (a[6],' ',a[7],' ',a[8])
-        if check_col(a) or check_row(a) or check_dia(a):
-            flag=1
+        check_col_pl(a,b,c,j)
+        check_row_pl(a,b,c,j)
+        check_diag_pl(a,b,c,j)
+        if j==1:
+            comp1(a,b,c)
+        if (check_col(a,b,c)==0 or check_row(a,b,c)==0 or check_diag(a,b,c)==0):
+            fl=1
             break
-        if i==4:
+        if(count==9):
             break
-        for j in range(9):
-            if a[j]=='X' or a[j]=='O':
-                continue
-            else:
-                x1=j+1
-                break;
-        print ("\nCOMPUTER PLAYED ON",x1)
-        a[x1-1]=ch1
-        print (a[0],' ',a[1],' ',a[2])
-        print (a[3],' ',a[4],' ',a[5])
-        print (a[6],' ',a[7],' ',a[8])
-        if check_col(a) or check_row(a) or check_dia(a):
-            flag=1
+        print('\n',a,'\n',b,'\n',c)
+        if (check_col(a,b,c)==0 or check_row(a,b,c)==0 or check_diag(a,b,c)==0):
+            fl=1
             break
-    if flag==0:
-        print ("MATCH DRAW!")
-        
-    n=int(input('Enter 1 to play another game or 0 to exit game : '))
-print ('Game over')
-
-
-
-
+    if(fl==0):
+        print('\nThe game ends in Tie')
+    print('-----------------------------------------------------------')
+    k=input('\nPress 1 if you want to play again press 1 else press 0: ')
+print('THE GAME IS OVER ')
--- one_player_tic_tac_toe_func.py
+++ one_player_tic_tac_toe_func.py
@@ -1,86 +1,236 @@
-def check_col(a):
-    if a[0]==a[3]==a[6]:
-        print ('CONGRATUALTIONS!',a[0],' WINNER!')
-        return 1
-    if a[1]==a[4]==a[7]:
-        print ('CONGRATUALTIONS!',a[1],' WINNER!')
-        return 1
-    if a[2]==a[5]==a[8]:
-        print ('CONGRATUALTIONS!',a[2],' WINNER!')
-        return 1
-def check_row(a):
-    if a[0]==a[1]==a[2]:
-        print ('CONGRATUALTIONS!',a[0],' WINNER!')
-        return 1
-    if a[3]==a[4]==a[5]:
-         print ('CONGRATUALTIONS!',a[3],' WINNER!')
-         return 1
-    if a[6]==a[7]==a[8]:
-        print ('CONGRATUALTIONS!',a[6],' WINNER!')
-        return 1
-def check_dia(a):
+import random
+def check_col(a,b,c):
+    for i in range(3):
+        if a[i]==b[i]==c[i]=='X':
+            print( '\nPlayer wins!!!')
+            return 0
+
+def check_row(a,b,c):
     for i in range(1):
-        if a[0]==a[4]==a[8] or a[2]==a[4]==a[6]:
-            print ('CONGRATUALTIONS!',a[4],' WINNER!')
-            return 1            
-def tic_tac_toe():
-    print ("\nENJOY THE GAME!!\n")
-    a=['1','2','3','4','5','6','7','8','9']
-    print (a[0],' ',a[1],' ',a[2])
-    print (a[3],' ',a[4],' ',a[5])
-    print (a[6],' ',a[7],' ',a[8])
-    flag=0
-    while 1:
-        print ("WHAT PLAYER1 WANTS TO TAKE 'X' or 'O'?")
-        ch=input()
-        if ch=='X' or ch=='O':
-            if ch=='X':
-                flagg=0
-                ch1='O'
-            else:
-                flagg=1
-                ch1='X'
-            break
-        else:
-            print ("ONLY TWO OPTIONS AVAILABLE TO CHOOSE!")
-    for i in range(5):
-        while 1:
-            x=input("ENTER LOCATION YOU WANT TO PLAY : ")
-            if x>9 or x<1:
-                print ("LOCATION NOT AVAILABLE!")
-                continue
-            elif a[x-1]=='X' or a[x-1]=='O':
-                print ("LOCATION ALREADY OCCUPIED!")
-            else:
-                a[x-1]=ch
-                break
-        print (a[0],' ',a[1],' ',a[2])
-        print (a[3],' ',a[4],' ',a[5])
-        print (a[6],' ',a[7],' ',a[8])
-        if check_col(a) or check_row(a) or check_dia(a):
-            flag=1
-            break
-        if i==4:
-            break
-        for j in range(9):
-            if a[j]=='X' or a[j]=='O':
-                continue
-            else:
-                x1=j+1
-                break;
-        print ("\nCOMPUTER PLAYED ON",x1)
-        a[x1-1]=ch1
-        print (a[0],' ',a[1],' ',a[2])
-        print (a[3],' ',a[4],' ',a[5])
-        print (a[6],' ',a[7],' ',a[8])
-        if check_col(a) or check_row(a) or check_dia(a):
-            flag=1
-            break
-    if flag==0:
-        print ("MATCH DRAW!")
-    print ("PRESS 'Y' TO PLAY AGAIN ")
-    ch=input()
-    if ch=='Y':
-        tic_tac_toe()
+        if a[i]==a[i+1]==a[i+2]=='X':
+            print( '\nPlayer wins!!!')
+            return 0
+        elif b[i]==b[i+1]==b[i+2]=='X':
+            print( '\nPlayer wins!!!')
+            return 0
+        elif c[i]==c[i+1]==c[i+2]=='X':
+            print( '\nPlayer wins!!!')
+            return 0
+        
+
+def check_diag(a,b,c):
+    for i in range(1):
+        if a[i]==b[i+1]==c[i+2]=='X':
+            print( '\nPlayer wins!!!')
+            return 0
+        elif c[i]==b[i+1]==a[i+2]=='X':
+            print( '\nPlayer wins!!!')
+            return 0
+        
+
+def comp1(a,b,c):
+    if b[1]!='X':
+        b[1]='O'
     else:
-        print ("THANKS FOR PLAYING THE GAME!")
+        k=random.choice([1,3,7,9])
+        l=random.choice([2,4,6,8])
+        if k==1 or k==3 and a[k-1]!='X' and a[k-1]!='O':
+            a[k-1]='O'
+        elif k==7 or k==9 and c[k-7]!='X' and a[k-7]!='O':
+            c[k-7]='O'
+        elif l==2 and a[1]!='O' and a[1]!='X':
+            a[1]='O'
+        elif l==8 and c[1]!='O' and c[1]!='X':
+            c[1]='O'
+        elif l==4 or l==6 and b[l-4]!='X' and b[l-4]!='O':
+            c[k-7]='O'
+
+    
+
+def player(a,b,c):
+    x=input('PLAYER : ')
+    x=int(x)
+    if(x<=3):
+        if a[x-1]=='O'or a[x-1]=='X':
+            print( 'sorry,choose someother digit.')
+            player1(a,b,c)    
+        else:
+            a[x-1]='X'
+    elif(x<=6):
+        if b[x-4]=='O'or b[x-4]=='X':
+            print( 'sorry,choose someother digit.')
+            player1(a,b,c) 
+        else:
+            b[x-4]='X'
+    elif(x<=9):
+        if c[x-7]=='O'or c[x-7]=='X':
+            print( 'sorry,choose someother digit.')
+            player1(a,b,c) 
+        else:
+            c[x-7]='X'
+
+
+def check_col_cmp(a,b,c):
+    for i in range(3):
+        if a[i]==b[i]=='O'and c[i]!='X' and c[i]!='O':
+            c[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif a[i]==c[i]=='O'and b[i]!='X' and b[i]!='O':
+            b[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif b[i]==c[i]=='O'and a[i]!='X' and a[i]!='O':
+            a[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+def check_row_cmp(a,b,c):
+    for i in range(1):
+        if a[i]==a[i+1]=='O' and a[i+2]!='X' and a[i+2]!='O':
+            a[i+2]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif a[i]==a[i+2]=='O'and a[i+1]!='X' and a[i+1]!='O':
+            a[i+1]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif a[i+1]==a[i+2]=='O' and a[i]!='X' and a[i]!='O':
+            a[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif b[i]==b[i+1]=='O' and b[i+2]!='X' and b[i+2]!='O':
+            b[i+2]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif b[i]==b[i+2]=='O'and b[i+1]!='X' and b[i+1]!='O':
+            b[i+1]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif b[i+1]==b[i+2]=='O' and b[i]!='X' and b[i]!='O':
+            b[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif c[i]==c[i+1]=='O' and c[i+2]!='X' and c[i+2]!='O':
+            c[i+2]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif c[i]==c[i+2]=='O'and c[i+1]!='X' and c[i+1]!='O':
+            c[i+1]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif c[i+1]==c[i+2]=='O' and c[i]!='X' and c[i]!='O':
+            c[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        else:
+            return 1
+        
+
+def check_diag_cmp(a,b,c):
+    for i in range(1):
+        if a[i]==b[i+1]=='O' and c[i+2]!='X' and c[i+2]!='O':
+            c[i+2]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif a[i]==c[i+2]=='O' and b[i+1]!='X' and b[i+1]!='O':
+            b[i+1]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif c[i+2]==b[i+1]=='O' and a[i]!='X' and a[i]!='O':
+            a[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif c[i]==b[i+1]=='O' and a[i+2]!='X' and a[i+2]!='O':
+            a[i+2]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif c[i]==a[i+2]=='O' and b[i+1]!='X' and b[i+1]!='O':
+            b[i+1]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0
+        elif a[i+2]==b[i+1]=='O' and c[i]!='X' and c[i]!='O':
+            c[i]='O'
+            print( a,'\n',b,'\n',c)
+            print( 'Computer wins!!!')
+            return 0        
+
+def check_col_pl(a,b,c,j):
+    for i in range(3):
+        if a[i]==b[i]=='X'and c[i]!='X' and c[i]!='O':
+            c[i]='O'
+            j=1
+        elif a[i]==c[i]=='X'and b[i]!='X' and b[i]!='O':
+            b[i]='O'
+            j=1
+        elif b[i]==c[i]=='X'and a[i]!='X' and a[i]!='O':
+            a[i]='O'
+            j=1
+def check_row_pl(a,b,c,j):
+    for i in range(1):
+        if a[i]==a[i+1]=='X' and a[i+2]!='X' and a[i+2]!='O':
+            a[i+2]='O'
+            j=1
+        elif a[i]==a[i+2]=='X'and a[i+1]!='X' and a[i+1]!='O':
+            a[i+1]='O'
+            j=1
+        elif a[i+1]==a[i+2]=='X' and a[i]!='X' and a[i]!='O':
+            a[i]='O'
+            j=1
+        elif b[i]==b[i+1]=='X' and b[i+2]!='X' and b[i+2]!='O':
+            b[i+2]='O'
+            j=1
+        elif b[i]==b[i+2]=='X'and b[i+1]!='X' and b[i+1]!='O':
+            b[i+1]='O'
+            j=1
+        elif b[i+1]==b[i+2]=='X' and b[i]!='X' and b[i]!='O':
+            b[i]='O'
+            j=1
+        elif c[i]==c[i+1]=='X' and c[i+2]!='X' and c[i+2]!='O':
+            c[i+2]='O'
+            j=1
+        elif c[i]==c[i+2]=='X'and c[i+1]!='X' and c[i+1]!='O':
+            c[i+1]='O'
+            j=1
+        elif c[i+1]==c[i+2]=='X' and c[i]!='X' and c[i]!='O':
+            c[i]='O'
+            j=1
+        
+
+def check_diag_pl(a,b,c,j):
+    for i in range(1):
+        if a[i]==b[i+1]=='X' and c[i+2]!='X' and c[i+2]!='O':
+            c[i+2]='O'
+            j=1
+        elif a[i]==c[i+2]=='X' and b[i+1]!='X' and b[i+1]!='O':
+            b[i+1]='O'
+            j=1           
+        elif c[i+2]==b[i+1]=='X' and a[i]!='X' and a[i]!='O':
+            a[i]='O'
+            j=1
+        elif c[i]==b[i+1]=='X' and a[i+2]!='X' and a[i+2]!='O':
+            a[i+2]='O'
+            j=1
+
+        elif c[i]==a[i+2]=='X' and b[i+1]!='X' and b[i+1]!='O':
+            b[i+1]='O'
+            j=1
+        elif a[i+2]==b[i+1]=='X' and c[i]!='X' and c[i]!='O':
+            c[i]='O'
+            j=1
